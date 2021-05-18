c796aa24774cd7ebd525a85102fe3f959e699852
Gen Hames <hamesg@tutanota.de>
2021-05-18 07:55:08 -0700

    Babel already uses ".macro" so I need something else. Here's ".acorn"

--- src/macros/style.macro/index.d.ts
+++
@@ -1,7 +0,0 @@
-/** Takeout css`` statement is replaced with a string of a unique classname */
-export declare function css(statics: TemplateStringsArray, ...variables: string[]): string;
-/** Takeout injectGlobal`` statement is removed entirely */
-export declare function injectGlobal(statics: TemplateStringsArray, ...variables: string[]): void;
-
-// Use `declare module 'style.macro' { const x: { ... } }` to define type
-// support for imports set in styleMacro's `importObject` option.
