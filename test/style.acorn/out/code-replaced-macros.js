0b382e10a6826ac7c0dfb6bb646e4fdf34a1e6d5
Gen Hames <hamesg@tutanota.de>
2021-06-05 13:04:52 -0700

    Bug: I'll need hashing and deduping...

--- test/style.acorn/out/code-replaced-macros.js
+++ test/style.acorn/out/code-replaced-macros.js
@@ -1,16 +1,16 @@
 // input.ts
 
 var exportedVariable = "#000";
-var styles = "css-[127,275)";
+var styles = "css-127";
 function shadow(css2) {
   return css2 + 10;
 }
 console.log(shadow(10));
 console.log(styles);
 console.log("pageBackground");
-var v1 = `m5 p5 ${"css-[421,448)"} align-center ${styles} ${"css-[?,?)"}`;
-var v2 = `m5 p5 ${"css-[515,542)"} align-center`;
-var v3 = `m5 p5 ${styles} ${"css-[587,614)"} align-center ${styles}`;
-var v4 = `${styles} ${"css-[663,690)"}`;
-var v5 = `${"css-[706,733)"}`;
-var v6 = `${"css-[749,776)"} hello`;
+var v1 = `m5 p5 ${"css-421"} align-center ${styles} ${"css-0"}`;
+var v2 = `m5 p5 ${"css-515"} align-center`;
+var v3 = `m5 p5 ${styles} ${"css-587"} align-center ${styles}`;
+var v4 = `${styles} ${"css-663"}`;
+var v5 = `${"css-706"}`;
+var v6 = `${"css-749"} hello`;
