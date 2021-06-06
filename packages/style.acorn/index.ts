b142c6a45b979f153443114716bd90919d724349
Gen Hames <hamesg@tutanota.de>
2021-06-06 16:20:56 -0700

    Generate index.d.ts at build time from index.ts

--- packages/style.acorn/index.ts
+++ packages/style.acorn/index.ts
@@ -1,2 +1,10 @@
 throw new Error('Importing style.acorn at runtime isn\'t supported. '
   + 'Use replaceMacros() from acorn-macros to preprocess your code.');
+
+/** Takeout css`` statement is replaced with a string of a unique classname */
+export declare function css(statics: TemplateStringsArray, ...variables: string[]): string;
+/** Takeout injectGlobal`` statement is removed entirely */
+export declare function injectGlobal(statics: TemplateStringsArray, ...variables: string[]): void;
+
+// Use `declare module 'style.acorn' { const x: { ... } }` to define type
+// support for imports set in styleMacro's `importObject` option.
