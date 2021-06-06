1d06902e2f8c0bf4a7756f724c87d8e0131a0f86
Gen Hames <hamesg@tutanota.de>
2021-06-06 16:17:38 -0700

    Better build script

---
+++ build.js
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+import esbuild from 'esbuild';
+
+const promises = [];
+
+function build(filePath) {
+  console.log(filePath);
+  const buildTask = esbuild.build({
+    entryPoints: [filePath],
+    outfile: filePath.replace('.ts', '.js'),
+  });
+  promises.push(buildTask);
+}
+for (const dirName of await fs.readdir('./packages')) {
+  if (!dirName.endsWith('.acorn')) continue;
+  for (const fileName of await fs.readdir(`./packages/${dirName}`)) {
+    if (fileName.endsWith('.d.ts') || !fileName.endsWith('.ts')) continue;
+    build(`./packages/${dirName}/${fileName}`);
+  }
+}
+await Promise.all(promises);
