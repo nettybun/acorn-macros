738b55d8d0dd8922dc5107092884f990b3b0d219
Gen Hames <hamesg@tutanota.de>
2021-06-20 15:23:23 -0700

    Fingers crossed on preval.acorn 🤞

--- packages/preval.acorn/src.ts
+++ packages/preval.acorn/src.ts
@@ -1,37 +1,39 @@
-import type { Macro } from 'acorn-macros';
-import { createContext, runInContext } from 'vm';
+import { AsyncFunction, MacroDefinition } from 'acorn-macros';
+import * as fs from 'fs';
+import * as path from 'path';
+// TODO: Add more?
 
-const prevalMacro = (): Macro => {
-
-  return {
-    importSource: 'preval.acorn',
-    importSpecifierImpls: {
-      // XXX: NO BUENO.
-      preval(code: string): unknown {
-        const context = createContext({ result: '', console });
-        const script = `(async () => {${code}})().then(res => { console.log('VM', res); result = res });`;
-        runInContext(script, context, {
-          microtaskMode: 'afterEvaluate',
-          // @ts-ignore Experimental
-          importModuleDynamically(specifer, script) {
-            console.log('VM IMD', specifer, script);
-          },
-        });
-        return context.result;
+const prevalMacro = (): MacroDefinition => ({
+  importSource: 'preval.acorn',
+  importSpecifiers: {
+    preval: {
+      rangeFn(_, ancestors) {
+        const nodeParent = ancestors[ancestors.length - 2];
+        const { type, start, end } = nodeParent; // Worst case this is "Program"
+        if (type !== 'TaggedTemplateExpression' && type !== 'CallExpression') {
+          throw new Error(`Macro preval must be called as either a function or a tagged template expression not ${type}`);
+        }
+        return { start, end };
+      },
+      async replaceFn({ identifier }, macroExpr) {
+        // There's never a naming conflicts with imports (fs, path, etc...) and
+        // the (minified) identifier - I remove it completely
+        const char = macroExpr[identifier.length];
+        if (char === '(') {
+          // Extract slice between >identifier("< and >")<
+          macroExpr = macroExpr.slice(identifier.length + 1, macroExpr.length - 2);
+        } else if (char === '`') {
+          // Extract slice between >identifier`< and >`<
+          macroExpr = macroExpr.slice(identifier.length, macroExpr.length - 1);
+        } else {
+          throw new Error(`Unexpected use of preval function:\n${macroExpr}`);
+        }
+        // TODO: Even provide globals at all? Let them import it?
+        const run = new AsyncFunction('fs', 'path', macroExpr);
+        return run(fs, path);
       },
     },
-    importSpecifierRangeFn: (specifier, ancestors) => {
-      if (specifier !== 'preval') {
-        throw new Error(`Unknown import "${specifier}" for preval.acorn`);
-      }
-      const nodeParent = ancestors[ancestors.length - 2];
-      const { type, start, end } = nodeParent; // Worst case this is "Program"
-      if (type !== 'TaggedTemplateExpression' && type !== 'CallExpression') {
-        throw new Error(`Macro preval must be called as either a function or a tagged template expression not ${type}`);
-      }
-      return { start, end };
-    },
-  };
-};
+  },
+});
 
 export { prevalMacro };
