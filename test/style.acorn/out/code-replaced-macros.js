affd76ad37173228215d02b1f184434217479899
Gen Hames <hamesg@tutanota.de>
2021-06-28 00:25:43 -0700

    Sync macros still work as expected

--- test/style.acorn/out/code-replaced-macros.js
+++ test/style.acorn/out/code-replaced-macros.js
@@ -1,16 +1,16 @@
 // input.ts
 
 var exportedVariable = "#000";
-var styles = "N3L86";
+var styles = "N4L86";
 function shadow(css2) {
   return css2 + 10;
 }
 console.log(shadow(10));
 console.log(styles);
 console.log("pageBackground");
-var v1 = `m5 p5 ${"N4L22"} align-center ${styles} ${"N1L18"}`;
-var v2 = `m5 p5 ${"N4L22"} align-center`;
-var v3 = `m5 p5 ${styles} ${"N4L22"} align-center ${styles}`;
-var v4 = `${styles} ${"N4L22"}`;
-var v5 = `${"N4L22"}`;
-var v6 = `${"N4L22"} hello`;
+var v1 = `m5 p5 ${"N3L22"} align-center ${styles} ${"N1L18"}`;
+var v2 = `m5 p5 ${"N3L22"} align-center`;
+var v3 = `m5 p5 ${styles} ${"N3L22"} align-center ${styles}`;
+var v4 = `${styles} ${"N3L22"}`;
+var v5 = `${"N3L22"}`;
+var v6 = `${"N3L22"} hello`;
