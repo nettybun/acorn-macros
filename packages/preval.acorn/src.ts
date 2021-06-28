c6d76505c88bd63017bb0fda02670f86661ea7ed
Gen Hames <hamesg@tutanota.de>
2021-06-28 00:24:59 -0700

    ASYNC PREVAL IS WORKING ✨

--- packages/preval.acorn/src.ts
+++ packages/preval.acorn/src.ts
@@ -1,7 +1,8 @@
-import { AsyncFunction, MacroDefinition } from 'acorn-macros';
-import * as fs from 'fs';
+import { AsyncFunction } from 'acorn-macros';
+import * as fs from 'fs/promises';
 import * as path from 'path';
-// TODO: Add more?
+
+import type { MacroDefinition } from 'acorn-macros';
 
 const prevalMacro = (): MacroDefinition => ({
   importName: 'preval.acorn',
@@ -16,24 +17,51 @@ const prevalMacro = (): MacroDefinition => ({
         return { start, end };
       },
       async replaceFn({ importSpecLocal: local }, macroExpr) {
-        // There's never a naming conflicts with imports (fs, path, etc...) and
+        // There's never a naming conflict with imports (fs, path, etc...) and
         // the (minified) identifier - I remove it completely
         const char = macroExpr[local.length];
         if (char === '(') {
           // Extract slice between >identifier("< and >")<
-          macroExpr = macroExpr.slice(local.length + 1, macroExpr.length - 2);
+          macroExpr = macroExpr.slice(local.length + 2, macroExpr.length - 2);
         } else if (char === '`') {
           // Extract slice between >identifier`< and >`<
-          macroExpr = macroExpr.slice(local.length, macroExpr.length - 1);
+          macroExpr = macroExpr.slice(local.length + 1, macroExpr.length - 1);
         } else {
           throw new Error(`Unexpected use of preval function:\n${macroExpr}`);
         }
-        // TODO: Even provide globals at all? Let them import it?
         const run = new AsyncFunction('fs', 'path', macroExpr);
-        return run(fs, path);
+        const result = (await run(fs, path)) as unknown;
+        // I don't want to write a serializer that infinitely expand objects.
+        // That's hard. There's circulary things and imagine trying to serialize
+        // something huge like "fs" as a module. No thanks.
+
+        if (result && typeof result === 'object') {
+          // Can't JSON.stringify because that silently drops things including
+          // functions. Also it returns strings, not code...
+          const pairs = Object.entries(result).map(([key, value]) => {
+            if (typeof value === 'function' && (value as {name: string}).name === key)
+              return String(value);
+            else
+              return `"${key}":${tryString(value)}`;
+          });
+          return `{${pairs.join(',')}}`;
+        }
+        // TODO: Add more serializations here like Set,Map,etc...
+
+        return String(result); // Hope your object serializes :^)
       },
     },
   },
 });
+
+function tryString(x: unknown) {
+  if (typeof x === 'string') return '"' + x.replaceAll('\n', '\\n') + '"';
+  const xString = String(x);
+  if (xString.startsWith('[object ')) {
+    console.error(x, `\nSerializes to "${xString}" which is probably wrong`);
+    throw new Error('Can\'t serialize the above preval result into JS code');
+  }
+  return xString;
+}
 
 export { prevalMacro };
--- test/preval.acorn/input.ts
+++ test/preval.acorn/input.ts
@@ -1,12 +1,22 @@
 import { preval } from 'preval.acorn';
 
 const content = preval`
-  const fs = await import('fs');
+  // By default 'fs/promises' and 'path' are imported
   return {
-    val: fs.readFileSync('./content.md', 'utf8'),
+    val: await fs.readFile('./content.md', 'utf8'),
     lineCount() { return content.val.split("\\n").length; }
   };
 ` as { val: string, lineCount: () => number };
+
+// Network requests at build time!
+preval`
+  console.log('✨ Fetching PNG ✨');
+  const fetch = (await import('node-fetch')).default;
+  fetch('https://placekitten.com/200/200')
+    .then(res => res.buffer())
+    .then(buffer => fs.writeFile('./out/cat.png', buffer));
+  return 'console.log("Cat pic at ./out/cat.png")';
+`;
 
 console.log(content.val);
 console.log(content.lineCount());
